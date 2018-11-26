7acbf0950e30dbebaae8cdb969329dfc37039139
oksana richards <oksana_vldk@hotmail.com>
2018-11-26 21:45:09 +0000

    add director file and run tests

---
+++ src/main/java/Management/Director.java
@@ -0,0 +1,15 @@
+package Management;
+
+import Staff.Employee;
+
+public class Director extends Employee {
+    private double budget;
+    public Director(String name, double salary, String numberNI, double budget){
+        super(name, salary, numberNI);
+        this.budget = budget;
+    }
+    public double getBudget(){
+        return this.budget;
+    }
+
+}
---
+++ src/test/java/DirectorTest.java
@@ -0,0 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DirectorTest {
+    Management.Director director;
+
+    @Before
+    public void before(){
+        director = new Management.Director("Bill Gales", 75000.00, "NP67854", 2000000.00);
+    }
+    @Test
+    public void canGetBudget(){
+        assertEquals(2000000.00, director.getBudget(), 0.01);
+    }
+    @Test
+    public void canGetName(){
+        assertEquals("Bill Gales", director.getName());
+    }
+    @Test
+    public void canGetSalary(){
+        assertEquals(75000.00, director.getSalary(), 0.01);
+    }
+    @Test
+    public void canGetNumberNI(){
+        assertEquals("NP67854", director.getNumberNI());
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        assertEquals(85000.00, director.raiseSalary(10000.00), 0.01);
+    }
+    @Test
+    public void canGetBonus(){
+        assertEquals(750, director.payBonus(), 0.01);
+    }
+}
